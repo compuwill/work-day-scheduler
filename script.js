c929e9ef4a4c6b238d1e2f638404fca763589067
Will Vorngsam <wpvorngsam@hotmail.com>
2022-01-30 22:32:57 +0000

    added saving functionality

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+var schedule = [];
+
+
 //set the current date
 var now = moment();
 $("#currentDay").text(now.format("dddd, MMMM Do"))
 
 //function for creating time blocks
-var createTimeBlock = function(time) {
-  
+var createTimeBlock = function(time,text) {
+  var momTime = moment(now,"L").set("hour",time).set("minute",0)
+
   var row = $("<div>").addClass("row timeblock mw-100")    
-    var hour = $("<span>").addClass("hour col-2 col-s-1").text(time.format("hA"))
+    var hour = $("<span>").addClass("hour col-2 col-s-1").text(momTime.format("hA"))
     var timeblock = $("<div>").addClass("").addClass("mw-100 col-8 col-s-10")
-      var description = $("<p>").addClass("description").text('test')
+      var description = $("<p>").addClass("description").text(text)
     var save = $("<btn>").addClass("saveBtn col-2 col-s-1 d-flex align-items-center justify-content-center").html("<i class='fas fa-save'></i>")
 
 
 
   //check time
-  if(time.isBefore(now))
+  if(momTime.isBefore(now))
     timeblock.addClass("past")
-  if(time.isAfter(now))
+  if(momTime.isAfter(now))
     timeblock.addClass("future")
-  if(time.isBefore(now) && time.add(1,'h').isAfter(now))
+  if(momTime.isBefore(now) && momTime.add(1,'h').isAfter(now))
     timeblock.addClass("present")
 
   row.append(hour)
@@ -29,12 +33,7 @@ var createTimeBlock = function(time) {
 }
 
 //on load, create the time blocks
-for(var i = 8; i <= 17; i++)
-{
-  var time = moment(now,"L").set("hour",i).set("minute",0)
-  createTimeBlock(time)
 
-}
 
 
 
@@ -51,10 +50,66 @@ $(".container").on("click", "p", function () {
 $(".container").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this).val().trim();
+  
+  // get the event's position in the list of other li elements
+  var index = $(this).closest(".row").index();
+  console.log(index)
+
 
   // recreate p element
-  var taskP = $("<p>").text(text);
+  var taskP = $("<p>").addClass("description").text(text);
+
+  //if the text was changed add the unsaved class
+  if(schedule[index].description != text)
+    taskP.addClass("unsaved")
+
+  schedule[index].description = text;
 
   // replace textarea with p element
   $(this).replaceWith(taskP);
 });
+
+//save button is clicked
+$(".container").on("click", "btn", function () {
+
+  $('p').each( function(index){ $(this).removeClass("unsaved")})
+
+  saveSchedule();
+});
+
+
+
+//Saving
+var saveSchedule = function () {
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+};
+
+//Loading
+var loadSchedule = function () {
+  schedule = JSON.parse(localStorage.getItem("schedule"));
+
+  // if nothing in localStorage, create a new object to track all scheduled events
+  if (!schedule) {
+    schedule = [];
+    for(var i = 8; i <= 17; i++)
+    {
+      var newTime = i;
+      var newDescription = "";
+      schedule.push({
+        time: newTime,
+        description: newDescription})
+    }
+
+  }
+
+  // loop over object properties
+    schedule.forEach(function (event) {
+      createTimeBlock(event.time,event.description)
+    });
+
+};
+
+
+// load schedule for the first time
+loadSchedule();
+console.log(schedule)
